ef9d7d86812c11c66f7ba492c4643e5bfcfec58c
Vinod Karmenghe <vinodkarmenghe@gmail.com>
2021-07-02 10:38:37 +0530

    Delete students.js

--- src/backend/students.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema();
-
-const StudentsSchema = new Schema({ name: String });
-
-const Student = mongoose.model('student', StudentsSchema);
-
-export default Student;
