6f0f3e1678922e5988d4f770c94e8b52509320de
Vinod Karmenghe <vinodkarmenghe@gmail.com>
2021-07-02 10:38:22 +0530

    Delete test_server.js

--- src/backend/test_server.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/amazon', { useNewUrlParser: true });
-
-mongoose.connection.once('open', () => {
-  console.log('connected');
-});
