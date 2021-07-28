a12a0ac90b59d7ec0744e5536720d591dfe73f1f
Vinod Karmenghe <vinodkarmenghe@gmail.com>
2021-07-28 17:20:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './JSPreperaton';
+import App from './frontend/App';
 
 import reportWebVitals from './reportWebVitals';
 
